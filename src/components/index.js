4255feff76180a7090504a2392350ec88111f241
Omar K <kalouo@pm.me>
2019-05-22 15:00:07 +0900

    creates file to route all component imports

---
+++ src/components/index.js
@@ -0,0 +1,8 @@
+import NavBar from "./NavBar";
+import NewTweet from "./NewTweet";
+import SignUp from "./SignUp";
+import Tweet from "./Tweet";
+import TweetsHeader from "./TweetsHeader";
+import TweetsManager from "./TweetsManager";
+
+export { NavBar, NewTweet, SignUp, Tweet, TweetsHeader,TweetsManager }
